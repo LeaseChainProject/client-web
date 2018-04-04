1cee8929395b9662e8e1530751769aeab906778e
altairmn <nukeballzy@gmail.com>
2018-04-04 14:27:12 -0400

    Changes made

--- src/components/LeaseForm/AddLeaseForm.js
+++ src/components/LeaseForm/AddLeaseForm.js
@@ -1,22 +1,32 @@
 import React from 'react';
 
 export default class AddLeaseForm extends React.Component {
-  state = {
-    // TODO
-    unit_id: '',
-    unit_address: '',
-    tenants: [],
-    lease_start_date: '',
-    lease_end_date: '',
-    monthly_rent: '',
-    riders: [],
-    selected_riders: [],
-    property_id: "",
-    property_id_options: ['ABC', 'DEF'],
-    ApartmentNumber: "",
-    ApartmentNumberOptions: [401, 402, 403, 404],
-    TenantEmail: "",
-    TenantEmailOptions: ["manocha@gmail.com"]
+  constructor(props) {
+    super(props)
+    this.state = {
+      // TODO
+      unit_id: '',
+      unit_address: '',
+      unit_type: '',
+      tenants: [],
+      lease_start_date: '',
+      lease_end_date: '',
+      monthly_rent: '',
+      riders: [],
+      selected_riders: [],
+      property_id: "",
+      property_id_options: [],
+      apartment_number: "",
+      apartment_number_options: [],
+      tenant_email: "",
+      tenant_email_options: [],
+      units_options: [],
+      filtered_units_options: []
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handlePropertyChange = this.handlePropertyChange.bind(this)
+    this.handleApartmentNumberChange = this.handleApartmentNumberChange.bind(this)
   }
 
   componentDidMount() {
@@ -37,23 +47,59 @@ export default class AddLeaseForm extends React.Component {
         property_id_options: property_ids
       }))
     })
+    fetch('http://localhost:3000/tenants', options)
+    .then((res) => (res.json()))
+    .then((tenants) => {
+      const tenant_emails = tenants.map(tenant => tenant.tenant_email)
+      console.log(tenant_emails)
+      this.setState((prevState) => ({
+        ...prevState,
+        tenant_email_options: tenant_emails
+      }))
+    })
+    fetch('http://localhost:3000/units', options)
+    .then((res) => (res.json()))
+    .then((units) => {
+      units = units.map(({property_id, unit_type, apartment_number}) => {
+        return {
+          property_id,
+          unit_type,
+          apartment_number
+        }
+      })
+      this.setState((prevState) => ({
+        ...prevState,
+        units_options: units
+      }))
+    })
   }
 
-  handleLeaseTerms = (lease_terms) => {
+
+  handleSubmit = (e) => {
     e.preventDefault();
-    // console.log("function called");
-    // lease_terms = e.target.elements;
+    // send the value in state
+    const options = {
+      method: 'POST',
+      body: JSON.stringify({
+        lease_start_date: this.state.lease_start_date,
+        lease_end_date: this.state.lease_end_date,
+        monthly_rent: this.state.monthly_rent,
+        property_id: this.state.property_id,
+        apartment_number: this.state.apartment_number,
+        unit_type: this.state.unit_type,
+        tenant_email: this.state.tenant_email,
+      }),
+      headers: {
+        'Content-Type': "application/json"
+      },
+      mode: 'cors'
+    }
 
-    // this.setState(() => ({
-    //   unit_id: lease_terms.unit_id.value,
-    //   unit_address: lease_terms.unit_address.value,
-    //   lease_start_date: lease_terms.lease_start_date.value,
-    //   lease_end_date: lease_terms.lease_end_date.value,
-    //   monthly_rent: lease_terms.rent.value
-    // }));
+    fetch('http://localhost:3000/add-lease', options)
+    .then((res) => console.log(res))
   }
 
-  handleInputChange = (e) => {
+  handleChange = (e) => {
     const name = e.target.name;
     const value = e.target.value;
 
@@ -62,23 +108,35 @@ export default class AddLeaseForm extends React.Component {
     }));
   }
 
-  handleAddTenant = (tenant_id) => {
-    this.setState((prevState) => ({
-      tenants: prevState.tenants.concat(tenant_id)
+  handlePropertyChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    const filtered_units_options = this.state.units_options.filter(unit => unit.property_id === value)
+    const apartment_number_options = filtered_units_options.map(unit => unit.apartment_number)
+    
+    this.setState(() => ({
+      [name]: value,
+      apartment_number_options,
+      filtered_units_options
     }));
   }
 
-  handleDeleteTenant = (tenantIDToRemove) => {
-    this.setState((prevState) => ({
-      tenants: prevState.tenants.filter((tenantID) => tenantIDToRemove !== tenantID)
-    }));
-  };
-
+  handleApartmentNumberChange = (e) => {
+    const name = e.target.name;
+    const value = parseInt(e.target.value, 10)
+    console.log(this.state.filtered_units_options)
+    const unit_type = this.state.filtered_units_options.filter((unit) => unit.apartment_number === value)[0].unit_type
+    this.setState({
+      [name]: value,
+      unit_type
+    })
+  }
 
   render() {
     return (
       <div className="col-lg-6 col-md-8 col-xs-10">
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <fieldset>
             <h4>Add a new lease record here.</h4>
             <br />
@@ -86,10 +144,10 @@ export default class AddLeaseForm extends React.Component {
               <label htmlFor="Property">Property</label>
                 <select
                  className="form-control"
-                 id="property_name"
-                 name="property_name"
-                 value={this.state.property_name}
-                 onChange={this.handleChange}>
+                 id="property_id"
+                 name="property_id"
+                 value={this.state.property_id}
+                 onChange={this.handlePropertyChange}>
                   {
                     this.state.property_id_options.map((property_id_option) => (
                       <option
@@ -104,19 +162,19 @@ export default class AddLeaseForm extends React.Component {
           </div>
 
           <div className="form-group">
-              <label htmlFor="ApartmentNumber">Apartment Number</label>
+              <label htmlFor="apartment_number">Apartment Number</label>
                 <select
                  className="form-control"
-                 id="ApartmentNumber"
-                 name="ApartmentNumber"
-                 value={this.state.ApartmentNumber}
-                 onChange={this.handleChange}>
+                 id="apartment_number"
+                 name="apartment_number"
+                 value={this.state.apartment_number}
+                 onChange={this.handleApartmentNumberChange}>
                   {
-                    this.state.ApartmentNumberOptions.map((ApartmentNumberOption) => (
+                    this.state.apartment_number_options.map((apartment_number_option) => (
                       <option
-                        key={ApartmentNumberOption}
-                        value={ApartmentNumberOption}>
-                        {ApartmentNumberOption}
+                        key={apartment_number_option}
+                        value={apartment_number_option}>
+                        {apartment_number_option}
                       </option>
                     ))
                   }
@@ -125,29 +183,29 @@ export default class AddLeaseForm extends React.Component {
           </div>
 
           <div className="form-group">
-              <label htmlFor="UnitType">Unit Type</label>
+              <label htmlFor="unit_type">Unit Type</label>
               <input type="text"
                      readOnly
                      className="form-control"
-                     id="UnitType"
-                     name="UnitType" 
-                     value={this.state.UnitType} />
+                     id="unit_type"
+                     name="unit_type" 
+                     value={this.state.unit_type} />
               <small id="UnitTypesmall">Type of the Unit</small>
           </div>
           <div className="form-group">
-              <label htmlFor="TenantEmail">Tenant e-mail</label>
+              <label htmlFor="tenant_email">Tenant e-mail</label>
               <select
                 className="form-control"
-                id="TenantEmail"
-                name="TenantEmail"
-                value={this.state.TenantEmail}
+                id="tenant_email"
+                name="tenant_email"
+                value={this.state.tenant_email}
                 onChange={this.handleChange}>
                 {
-                  this.state.TenantEmailOptions.map((TenantEmailOption) => (
+                  this.state.tenant_email_options.map((tenant_email_option) => (
                     <option
-                      key={TenantEmailOption}
-                      value={TenantEmailOption}>
-                      {TenantEmailOption}
+                      key={tenant_email_option}
+                      value={tenant_email_option}>
+                      {tenant_email_option}
                     </option>
                   ))
                 }
@@ -155,43 +213,33 @@ export default class AddLeaseForm extends React.Component {
               <small id="Tenantemailsmall">Tenant e-mail</small>
           </div>
           <div className="form-group">
-              <label htmlFor="LeaseType">Deposit Amount</label>
-              <input type="text"
-                     className="form-control"
-                     id="LeaseType"
-                     name="LeaseType" 
-                     value={this.state.LeaseType}
-                     onChange={this.handleInputChange} />
-              <small id="LeaseTypesmall">Deposit Amount</small>
-          </div>
-          <div className="form-group">
-              <label htmlFor="LeaseFromDate">Lease From Date</label>
+              <label htmlFor="lease_start_date">Lease From Date</label>
               <input type="date"
                      className="form-control"
-                     id="LeaseFromDate"
-                     name="LeaseFromDate" 
-                     value={this.state.LeaseFromDate}
-                     onChange={this.handleInputChange} />
+                     id="lease_start_date"
+                     name="lease_start_date" 
+                     value={this.state.lease_start_date}
+                     onChange={this.handleChange} />
               <small id="LeaseFromDatesmall">Lease Start Date</small>
           </div>
           <div className="form-group">
-              <label htmlFor="LeaseToDate">Lease To Date</label>
+              <label htmlFor="lease_end_date">Lease To Date</label>
               <input type="date"
                      className="form-control"
-                     id="LeaseToDate"
-                     name="LeaseToDate" 
-                     value={this.state.LeaseToDate}
-                     onChange={this.handleInputChange} />
+                     id="lease_end_date"
+                     name="lease_end_date" 
+                     value={this.state.lease_end_date}
+                     onChange={this.handleChange} />
               <small id="LeaseToDatesmall">Lease End Date</small>
           </div>
           <div className="form-group">
-              <label htmlFor="MonthlyRent">Monthly Rent</label>
+              <label htmlFor="monthly_rent">Monthly Rent</label>
               <input type="text"
                      className="form-control"
-                     id="MonthlyRent"
-                     name="MonthlyRent" 
-                     value={this.state.MonthlyRent}
-                     onChange={this.handleInputChange} />
+                     id="monthly_rent"
+                     name="monthly_rent" 
+                     value={this.state.monthly_rent}
+                     onChange={this.handleChange} />
               <small id="MonthlyRentsmall">Rent per month</small>
           </div>
         <button type="submit" className="btn btn-primary">Submit</button>&nbsp;&nbsp;&nbsp;<button type="submit" className="btn btn-secondary">Preview</button>
--- src/components/TenantForm/AddTenantForm.js
+++ src/components/TenantForm/AddTenantForm.js
@@ -1,41 +1,130 @@
 import React from 'react'
 
 export default class AddTenantForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tenant_name: '',
+      tenant_dob: '',
+      tenant_email: '',
+      tenant_phone: '',
+      income: '',
+      credit_score: ''
+    }
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    // send the value in state
+    const options = {
+      method: 'POST',
+      body: JSON.stringify({
+        tenant_name: this.state.tenant_name,
+        tenant_dob: this.state.tenant_dob,
+        tenant_email: this.state.tenant_email,
+        tenant_phone: this.state.tenant_phone,
+        income: this.state.income,
+        credit_score: this.state.credit_score 
+      }),
+      headers: {
+        'Content-Type': "application/json"
+      },
+      mode: 'cors'
+    }
+
+    fetch('http://localhost:3000/add-tenant', options)
+    .then((res) => console.log(res))
+  }
+
+
+
   render() {
     return (
       <div className="col-lg-6 col-md-8 col-xs-10">
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <fieldset>
             <h4>Tenant Information</h4>
             <br/>
             <div className="form-group">
               <label>Tenant Name</label>
-              <input type="text" className="form-control" id="TenantName" name="TenantName" />
+              <input
+                type="text"
+                className="form-control"
+                id="tenant_name"
+                name="tenant_name"
+                value={this.state.tenant_name}
+                onChange={this.handleChange}
+                />
               <small>Name of the Tenant</small>
             </div>
             <div className="form-group">
               <label>Tenant DOB</label>
-              <input type="date" className="form-control" id="TenantDOB" name="TenantDOB" />
+              <input
+                type="date"
+                className="form-control"
+                id="tenant_dob"
+                name="tenant_dob"
+                value={this.state.tenant_dob}
+                onChange={this.handleChange}
+              />
               <small>Date of Birth of the Tenant</small>
             </div>
             <div className="form-group">
               <label>Tenant Phone</label>
-              <input type="text" className="form-control" id="TenantPhone" nane="TenantPhone" />
+              <input
+                type="text"
+                className="form-control"
+                id="tenant_phone"
+                name="tenant_phone"
+                value={this.state.tenant_phone}
+                onChange={this.handleChange}
+                />
               <small>Phone number</small>
             </div>
             <div className="form-group">
               <label>Tenant Email</label>
-              <input type="email" className="form-control" id="TenantEmail" name="TenantEmail" />
+              <input
+                type="email"
+                className="form-control"
+                id="tenant_email"
+                name="tenant_email"
+                value={this.state.tenant_email}
+                onChange={this.handleChange}
+                />
               <small>Email</small>
             </div>
             <div className="form-group">
               <label>Income</label>
-              <input type="text" className="form-control" id="TenantIncome" name="TenantIncome" />
+              <input
+                type="text"
+                className="form-control"
+                id="income"
+                name="income"
+                value={this.state.income}
+                onChange={this.handleChange}
+                />
               <small>Income of the tenant (per annum)</small>
             </div>
             <div className="form-group">
               <label>Credit Score</label>
-              <input type="text" className="form-control" id="TenantCreditScore" name="TenantCreditScore" />
+              <input
+                type="text"
+                className="form-control"
+                id="credit_score"
+                name="credit_score"
+                value={this.state.credit_score}
+                onChange={this.handleChange}
+                />
               <small>Credit Score</small>
             </div>
         <button type="submit" className="btn btn-primary">Submit</button>&nbsp;&nbsp;&nbsp;<button type="submit" className="btn btn-secondary">Preview</button>
